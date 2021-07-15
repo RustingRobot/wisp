e382ddeffbc607b59e7d5c7cd15d55a14d2d60c5
RustingRobot <rusting.robot.scratch@gmail.com>
2021-07-15 14:04:14 +0200

    Add project files.

---
+++ wisp/wispEngine.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace wisp
+{
+    public class wispEngine : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public wispEngine()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            Debug.WriteLine("wisp update baby!!");
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+
+        public void ping()
+        {
+            Debug.WriteLine("pong");
+        }
+    }
+}
